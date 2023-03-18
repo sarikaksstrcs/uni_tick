d0974f29a691503dcaed78de0bf0b54ab7e6da8e
sarikaksstrcs <sarika.ks.official@gmail.com>
2023-03-19 04:54:54 +0530

    s

--- ticket_booking/urls.py
+++ ticket_booking/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, event_details, book_ticket,event
+from .views import index, event_details, book_ticket,event,send_email
 app_name = 'admin'
 
 urlpatterns = [
@@ -7,7 +7,7 @@ urlpatterns = [
     path('event/', event, name='event'),
     path('event/<int:id>/', event_details, name='event_details'),
     path('event/<int:id>/book_ticket', book_ticket, name='book_ticket'),
-    #path('qr/<int:id>/',qr_code,name='qr_code'),
+    path('qr',send_email,name='send_email'),
     
 ]
 
